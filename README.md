7fb64430b7999ed7a05c304535d34a1e3dc83a82
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-15 16:21:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Statify Node App
---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+app.use(express.json());
+
+app.listen(process.env.PORT || 4000);
